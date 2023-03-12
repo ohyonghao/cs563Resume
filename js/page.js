a963b56be6c20435d2c90932a9c09f7cf570c712
David Post <david@ohyonghao.com>
2023-03-11 22:09:41 -0800

    Added in previous experinece
    Created a addJob function to facilitate most of this, and just generate
    the content in a clean format at the top of the generatePrevious()
    function.
    
    Signed-off-by: David Post <david@ohyonghao.com>

--- js/page.js
+++ js/page.js
@@ -55,14 +55,76 @@ function generateAbout() {
     app.appendChild(div);
 }
 function generatePrevious() {
+    // Main content
+    job1descr = [
+        `UPS was my first job which I got while I was working in highschool.`,
+    ];
+    job2descr = [
+        `While working at Baard Energy I learned a lot about computer science. I had worked 
+    there for about two years before leaving to go and live in Taiwan for six years.`,
+        `Projects that I had worked on there included mass email, phone PBXs, and ERP systems.`,
+    ];
+    job3descr = [
+        `I worked here for five years as a contractor on two different teams. The first team was an 
+        Android feature team for a technology called WiDi.`,
+        `The second team I worked on was an Intel lab that was essentially an internal kickstarter.
+        People with ideas would come to us and we would create prototypes of these ideas to be 
+        demonstrated internally to see if it would be worth developing further.`,
+    ];
+    job4descr = [
+        `ICS is a pretty exciting place to work at. One thing you have to understand is the nature
+        of the work. As a software consulting firm that specializes in UI development our projects
+        tend to be the final touches on systems.`,
+        `I've worked on systems from railway to aerospace, along with medical devices and chemical
+        analysis.`,
+    ];
+
     // Grab the main section
     let app = clearApp();
 
-    let p = document.createElement("p");
-    p.textContent = `Previous work experience:`;
-
-    app.appendChild(p);
+    let jobdiv = document.createElement("div");
+    jobdiv.setAttribute("class", "container");
+    let rowdiv = document.createElement("div");
+    rowdiv.setAttribute("class", "row row-cols-auto");
+    rowdiv.append(addJob("job1", "UPS", "Sorter", job1descr));
+    rowdiv.append(addJob("job2", "Baard Energy", "Code Monkey", job2descr));
+    rowdiv.append(addJob("job3", "Intel", "Gate Keeper", job3descr));
+    rowdiv.append(
+        addJob(
+            "job4",
+            "Integrated Computer Solutions",
+            "Software Engineer",
+            job4descr
+        )
+    );
+    jobdiv.appendChild(rowdiv);
+    app.appendChild(jobdiv);
 }
+
+function addJob(id, name, position, other) {
+    job = document.createElement("div");
+    job.setAttribute("class", "figure text-wrap col-md");
+    let h2 = document.createElement("h2");
+    h2.textContent = name;
+
+    let pos = document.createElement("h3");
+    pos.setAttribute("class", "text-wrap");
+    pos.textContent = "Position: " + position;
+
+    job.appendChild(h2);
+    job.appendChild(pos);
+
+    other.forEach((text) => {
+        let descr = document.createElement("p");
+        descr.setAttribute("id", id);
+        descr.setAttribute("class", "text-wrap");
+        descr.textContent = text;
+        job.appendChild(descr);
+    });
+
+    return job;
+}
+
 function generateProjects() {
     clearApp();
 }
