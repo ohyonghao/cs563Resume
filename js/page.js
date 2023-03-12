04f707f8cdcb71faffd48be4ffd0ed6d2fe228d1
David Post <david@ohyonghao.com>
2023-03-11 21:06:28 -0800

    Added in anchors to content
    Now the Navbar will navigate to the anchored element.
    
    Signed-off-by: David Post <david@ohyonghao.com>

--- js/page.js
+++ js/page.js
@@ -7,9 +7,14 @@ function generateNavbar(pages) {
     pages.forEach((callback, name) => {
         let li = document.createElement("li");
         li.setAttribute("class", "list-item");
-        li.setAttribute("id", name);
+        li.setAttribute("id", "nav" + name);
         li.setAttribute("onclick", callback);
         li.textContent = name;
+
+        let a = document.createElement("a");
+        a.setAttribute("class", "nav-link");
+        a.setAttribute("href", "#" + name);
+        li.appendChild(a);
         ul.appendChild(li);
     });
 
@@ -26,6 +31,18 @@ function generateAbout() {
     // Grab the main section
     let app = clearApp();
 
+    let div = document.createElement("div");
+    div.setAttribute("id", "About");
+
+    // Img
+    let img = document.createElement("img");
+    img.setAttribute("src", "img/dpost.webp");
+    img.setAttribute(
+        "alt",
+        "Headshot of a handsome David Post, with dark brown hair and hazel eyes."
+    );
+    img.setAttribute("class", "headshot");
+
     let p = document.createElement("p");
     p.textContent = `Good news everyone! My site is now live here on Github Pages. Allow me to
     briefly introduce myself. My name is David Post. I am a graduate student
@@ -33,7 +50,9 @@ function generateAbout() {
     2020 with a Bachelors of Science in Computer Science and Mathematics,
     while minoring in Physics.`;
 
-    app.appendChild(p);
+    div.appendChild(img);
+    div.appendChild(p);
+    app.appendChild(div);
 }
 function generatePrevious() {
     // Grab the main section
